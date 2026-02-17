ba11294edc3cebcedc58cc066dbbedf09a2221ef
bambsdev <ibrohimsairony@gmail.com>
2026-02-17 21:05:12 +0700

    Add Tokio dependency and implement concurrency tests with async/await

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,18 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use std::{
+        cell::RefCell,
         sync::{
+            Arc, Barrier, Mutex, Once,
             atomic::{AtomicI32, Ordering},
             mpsc::channel,
         },
         thread::{Builder, current, sleep, spawn},
         time::Duration,
     };
+
+    use tokio::runtime::Runtime;
+
     // ! Chapter 1 : Membuat thread : spawn()
 
     #[test]
@@ -103,7 +108,7 @@ mod tests {
         let handle = spawn(closure);
         handle.join().unwrap();
 
-        // println!("{}", name); // Error karena owner sudah dipindahkan
+        // println!("{}", user); // Error karena owner sudah dipindahkan
     }
     // ! Chapter 4 : current thread
     // thread bersumber dari spawn tidak punya nama.
@@ -231,14 +236,15 @@ mod tests {
 
     // ! Chapter 8 : Atomic
 
-    static OTHER_COUNTER: AtomicI32 = AtomicI32::new(0);
     #[test]
     fn test_atomic() {
+        static COUNTER: AtomicI32 = AtomicI32::new(0);
+
         let mut handles = vec![];
         for _ in 0..10 {
             let handle = spawn(|| {
                 for _ in 0..10000000 {
-                    OTHER_COUNTER.fetch_add(1, Ordering::Relaxed);
+                    COUNTER.fetch_add(1, Ordering::Relaxed);
                 }
             });
             handles.push(handle);
@@ -247,6 +253,185 @@ mod tests {
         for handle in handles {
             handle.join().unwrap()
         }
-        println!("Counter : {}", OTHER_COUNTER.load(Ordering::Relaxed));
+        println!("Counter : {}", COUNTER.load(Ordering::Relaxed));
+    }
+
+    // ! Chapter 9 : Atomic Rc / Arc / Atomic Reference counted
+
+    #[test]
+    fn test_atomic_reference() {
+        let counter = Arc::new(AtomicI32::new(0));
+
+        let mut handles = vec![];
+        for _ in 0..10 {
+            let counter_clone = Arc::clone(&counter);
+            let handle = spawn(move || {
+                for _ in 0..10000000 {
+                    counter_clone.fetch_add(1, Ordering::Relaxed);
+                }
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap()
+        }
+        println!("Counter : {}", counter.load(Ordering::Relaxed));
+    }
+
+    // ! Chapter 10 : Mutex
+    #[test]
+    fn test_mutex() {
+        let counter = Arc::new(Mutex::new(0));
+
+        let mut handles = vec![];
+        for _ in 0..10 {
+            let counter_clone = Arc::clone(&counter);
+            let handle = spawn(move || {
+                for _ in 0..10000000 {
+                    let mut data = counter_clone.lock().unwrap();
+                    *data += 1;
+                }
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap()
+        }
+        println!("Counter : {}", *counter.lock().unwrap());
+    }
+
+    // ! Chapter 11 : local Thred -> alur hidup yang mengikuti threadnya
+    thread_local! {
+        pub static NAME : RefCell<String> = RefCell::new("Default".to_string())
+    }
+    thread_local! {
+        pub static OTHER_NAME : RefCell<String> = RefCell::new("Default".to_string())
+    }
+    #[test]
+    fn test_local_thread() {
+        let handle = spawn(|| {
+            NAME.with_borrow_mut(|name| {
+                *name = "Budi".to_string();
+            });
+
+            NAME.with_borrow(|name| {
+                println!("{}", name);
+            })
+        });
+        handle.join().unwrap();
+        NAME.with_borrow(|name| {
+            println!("{}", name);
+        })
+    }
+    // ! Chapter 12 : thread panic -> panic terisolasi di thread tersebut
+    #[test]
+    fn test_thread_panic() {
+        let handle = spawn(|| panic!("oops, someting went wrong"));
+        match handle.join() {
+            Ok(_) => println!("Thread Finish"),
+            Err(_) => println!("Thread Panic"),
+        }
+    }
+    // ! Chapter 13 : barrier -> thread menunggu supaya semua thread siap, setelah itu jalan bareng, seperti balapan lari.
+    #[test]
+    fn test_barrier() {
+        let barrier = Arc::new(Barrier::new(10));
+        let mut handles = vec![];
+        for i in 0..10 {
+            let barrier_clone = Arc::clone(&barrier);
+            let handle = spawn(move || {
+                println!("Join Game-{}", i);
+                barrier_clone.wait();
+                println!("Game-{} Start", i);
+            });
+            handles.push(handle);
+        }
+    }
+    // ! Chapter 13 : Once -> hanya satu thread pertama yang bisa menginisialisasi datanya.
+    static mut TOTAL_COUNTER: i32 = 0;
+    static TOTAL_INIT: Once = Once::new();
+
+    fn get_total() -> i32 {
+        unsafe {
+            TOTAL_INIT.call_once(|| {
+                TOTAL_COUNTER += 1;
+            });
+            return TOTAL_COUNTER;
+        }
+    }
+    #[test]
+    fn test_once() {
+        let mut handles = vec![];
+        for _ in 0..10 {
+            let handle = spawn(|| {
+                let total = get_total();
+                println!("Total : {}", total);
+            });
+            handles.push(handle);
+        }
+        for handle in handles {
+            handle.join().unwrap()
+        }
+    }
+    // ! Chapter 14 : async await. menggunakan tokio sebagai runtime/executor nya.
+    async fn get_async_data() -> String {
+        tokio::time::sleep(Duration::from_secs(2)).await;
+        println!("Hello from async");
+        return "Hello from async".to_string();
+    }
+
+    #[tokio::test]
+    async fn test_async() {
+        let _function = get_async_data();
+        println!("Finish call async");
+        let data = get_async_data().await;
+        println!("{}", data);
+    }
+
+    // ! Chapter 14 : tast -> implementasi cuncarrency. pakai tokio::spawn()
+    async fn get_database(wait: u64) -> String {
+        println!("{:?} get database data", current().id());
+        tokio::time::sleep(Duration::from_secs(wait)).await;
+        println!("{:?} hello from database", current().id());
+        "Hello From Database".to_string()
+    }
+    #[tokio::test]
+    async fn test_cuncarrency() {
+        let mut handles = vec![];
+        for i in 0..5 {
+            let handle = tokio::spawn(get_database(i));
+            handles.push(handle);
+        }
+        for handle in handles {
+            let data = handle.await.unwrap();
+            println!("Response : {}", data);
+        }
+    }
+
+    // ! Chapter 14 : tokio runtime -> custom runtimenya tokio biar lebih gahar / implementasi thread + task
+    async fn run_concurrency_and_parallel(runtime: Arc<Runtime>) {
+        let mut handles = vec![];
+        for i in 0..5 {
+            let handle = runtime.spawn(get_database(i));
+            handles.push(handle);
+        }
+        for handle in handles {
+            let data = handle.await.unwrap();
+            println!("Response : {}", data);
+        }
+    }
+
+    #[test]
+    fn test_tokio_runtime() {
+        let runtime = Arc::new(
+            tokio::runtime::Builder::new_multi_thread()
+                .worker_threads(10)
+                .enable_time()
+                .build()
+                .unwrap(),
+        );
+        runtime.block_on(run_concurrency_and_parallel(Arc::clone(&runtime)))
     }
 }
